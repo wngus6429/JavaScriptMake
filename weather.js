a34035acbe5c3c820004704ce2f1b13296bff68f
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-01-04 18:57:37 +0900

    20200104
    20200104

--- weather.js
+++ weather.js
@@ -1,7 +1,24 @@
 //https://home.openweathermap.org/api_keys
 
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "06da9d5068ee6f135e90b69ebe5ea456";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+      //콘솔이나 network에서 날아온 정보를 가지고 적어야함.
+    });
+} //마지막에 API_KEY 넣어줘야함、units=metri、이거는 화씨인가 뭐시기 온도 표시 바꿈
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -15,6 +32,7 @@ function handleGeoSucces(position) {
   //이렇게 가능
   saveCoords(coordsObj);
   //위도와 경도를 읽어온다.
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
