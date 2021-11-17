677115cd57bc2198b7a3b17220e1df77aa51cae8
ParkJuhyun <wngus6429@gmail.com>
2021-11-18 00:37:15 +0900

    f
    f

--- clock.js
+++ clock.js
@@ -8,9 +8,7 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
   //이렇게 해야 분, 초 앞에 0이 붙는다.
 }
 
